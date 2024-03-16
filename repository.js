185882d1702eb79cadc9041e6bf21e6671c03c3d
deesadiasa <163534875+deesadiasa@users.noreply.github.com>
2024-03-16 12:56:27 +0800

    Add files via upload

---
+++ repository.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+var firebaseConfig = {
+    apiKey: "AIzaSyBsTJb-TOHR-_VPgxnG_uqeGS1_2kpEdtU",
+    authDomain: "aci-registrar.firebaseapp.com",
+    projectId: "aci-registrar",
+    storageBucket: "aci-registrar.appspot.com",
+    messagingSenderId: "49882101284",
+    appId: "1:49882101284:web:b511700ced8288972d4ab1",
+    measurementId: "G-63VYPB0CXY"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
